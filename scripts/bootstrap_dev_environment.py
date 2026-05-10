f32b2807baed97e9de325bbfedea8af83a7ab108
Marco Coppola <marco.n.coppola@gmail.com>
2026-05-10 17:21:46 +0200

    chore(git): fixed script for addition to repo snapshot of issues and milestones

--- scripts/bootstrap_dev_environment.py
+++ scripts/bootstrap_dev_environment.py
@@ -272,10 +272,6 @@ def git_local_config_entries() -> list[tuple[str, str]]:
         ),
         (
             "alias.txz",
-            """!f(){ name="${1:-repo}"; tmp="$(mktemp -d)"; trap 'rm -rf "$tmp"' EXIT; mkdir -p "$tmp/repo"; git ls-files -z | XZ_OPT="-9e -T0" tar --null -T - -cJf "$PWD/$name.tar.xz" --transform='s,^,repo/,'; }; f""",
-        ),
-        (
-            "alias.txz",
             """!f() { name="${1:-repo}"; tmp="$(mktemp -d)"; trap 'rm -rf "$tmp"' EXIT; mkdir -p "$tmp/repo"; { git ls-files -z; printf "%s\0" issues.json milestones.json; } | XZ_OPT="-9e -T0" tar --null -T - -cJf "$PWD/$name.tar.xz" --transform='s,^,repo/,'; }; f""",
         ),
         (
